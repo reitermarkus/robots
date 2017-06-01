0edc3180820d2bfc251666eefbca4f58cc2a25b4
Markus Reiter <me@reitermark.us>
2017-06-01 15:59:11 +0200

    Add `line` method.

--- robots.cr
+++ robots.cr
@@ -43,3 +43,32 @@ puts "   " + antenna(0,0) + antenna(0,1) + "   "
 puts "  " + head(0) + "  "
 puts " " + hand(0) + body(0) + hand(1) + " "
 puts foot(0)
+
+def line(number, action)
+  [
+    "You put your ? in",
+    "You take your ? out",
+    "You put your ? in",
+    "And you shake it all about",
+    "You do the Hokey Pokey and you turn yourself around",
+    "That's what it's all about!",
+  ].map { |l| l.sub('?', ACTIONS[action % 5]) }[number % 6]
+end
+
+ACTIONS = [
+  "right foot",
+  "left foot",
+  "right hand",
+  "left hand",
+  "whole self",
+]
+
+i = 0
+while true
+  6.times { |l|
+    puts line(l, i)
+    sleep 0.5
+  }
+  sleep 0.5
+  i += 1
+end
